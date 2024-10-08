267ad078c8859a56f641a443c9253a92ebb86b40
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-10-08 16:24:29 +0700

    fix processing_data.py

--- test.py
+++ test.py
@@ -59,63 +59,11 @@ processing_data = Preprocessing(window_size=1,
                                 temp_df=temp_df, 
                                 hr_df=hr_df, 
                                 gsr_df=gsr_df, 
-                                rr_df=rr_df, 
+                                rr_df=rr_df,
+                                label_df = label_df,
                                 normalize="Standard")
 
-# Lấy dữ liệu
-data = processing_data.get_data()
-
-test_ids = ['3caqi','6frz4','bd47a','f1gjp','iz3x1']
-
-train_ids = ['1mpau', '2nxs5', '5gpsc', '7swyk', '8a1ep', 'b7mrd',
-       'c24ur', 'dkhty', 'e4gay', 'ef5rq', 'f3j25', 'hpbxa',
-       'ibvx8', 'iz2ps', 'rc1in', 'tn4vl', 'wjxci', 'yljm5']
-
-X_train = []
-y_train = []
-X_test = []
-y_test = []
-user_train = []
-user_test = []
-
-
-for user in label_df.user_id.unique():
-    if user in train_ids:
-        user_features = data[label_df.user_id == user]
-        X_train.append(user_features)
-        y = label_df.loc[label_df.user_id == user, 'level'].values
-        
-        # Convert labels (rest,0,1,2) to binary (rest vs task)
-        y[y == 'rest'] = -1
-        y = y.astype(int) + 1
-        y[y > 0] = 1
-        y_train.extend(y)
-        
-        temp = label_df.loc[label_df.user_id==user,'user_id'].values #labels
-        user_train.extend(temp)
-    elif user in test_ids:
-        user_features = data[label_df.user_id == user]
-        X_test.append(user_features)
-        y = label_df.loc[label_df.user_id == user, 'level'].values
-        
-        # Convert labels (rest,0,1,2) to binary (rest vs task)
-        y[y == 'rest'] = -1
-        y = y.astype(int) + 1
-        y[y > 0] = 1
-        y_test.extend(y)
-        
-        temp = label_df.loc[label_df.user_id==user,'user_id'].values #labels
-        user_test.extend(temp)
-
-# Concatenate and convert to DataFrame/NumPy array
-X_train = pd.concat(X_train)
-y_train = np.array(y_train)
-X_test = pd.concat(X_test)
-y_test = np.array(y_test)
-
-print('Train data:', X_train.shape, y_train.shape)
-print('Test data:', X_test.shape, y_test.shape)
+X_train, y_train, X_test, y_test, user_train, user_test = processing_data.get_data()
 
 X_train, X_test = Feature_Selection.selected_RFECV(X_train, X_test, y_train, user_train)
 print(X_train)
-
