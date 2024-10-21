4b52d38c4f3445b5b77729db4bc343481c20fa12
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-10-21 08:47:43 +0700

    fix Test

--- test.py
+++ test.py
@@ -106,7 +106,7 @@ if not os.path.exists(directory_name):
     os.makedirs(directory_name)
 
 # create folder with timestamp
-timestamp = datetime.now().strftime('%Y/%m/%d')
+timestamp = datetime.now().strftime('%Y_%m_%d')
 sub_directory = directory_name + f"_{timestamp}"
 os.makedirs(sub_directory)
 
