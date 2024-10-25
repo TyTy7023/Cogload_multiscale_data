cc765f19a472d033d9ca061c6223957597776252
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-10-25 17:17:41 +0700

    fix model

--- model.py
+++ model.py
@@ -104,8 +104,11 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
 
         # Dự đoán trên tập kiểm tra
         if model == 'ESVM':
-            e, best_param = useModel(model)
-        print(f"Best parameters found: {best_model.best_params_ if model != 'ESVM' else best_param}\n" )
+            _, best_param = useModel(model)
+            print(f"Best parameters found: {best_param}\n")
+        # Ngược lại, nếu best_model có best_params_, in giá trị của nó
+        elif hasattr(best_model, 'best_params_'):
+            print(f"Best parameters found: {best_model.best_params_}\n")
         y_pred = best_model.predict(X_test)
         y_pred_proba = best_model.predict_proba(X_test)
 
