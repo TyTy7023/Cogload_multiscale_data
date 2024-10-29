a6449f8007e1eaa550dddba1bd2d018a7f52a166
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-10-29 14:28:22 +0700

    fix best model

--- best_model.py
+++ best_model.py
@@ -64,7 +64,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
     # ROC tâp validation K-Fold
     EDA.draw_ROC(path, y_vals, y_pred_vals, 'LDA')
 
-    print(f"Best parameters found: {best_model.best_params_}\n")
+    print(f"Best parameters found: {best_model.get_params()}\n")
     y_pred = best_model.predict(X_test)
     y_pred_proba = best_model.predict_proba(X_test)
 
