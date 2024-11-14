9e0731ba40aca75bb76f24c2bcca73aaac2a0732
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-11-14 12:49:59 +0700

    change local variable model to gobal var

--- model/single_model.py
+++ model/single_model.py
@@ -24,9 +24,8 @@ import sys
 sys.path.append('/kaggle/working/cogload/')
 from EDA import EDA
 
-def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 , debug = 0):
+def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 , debug = 0, models = ['LNR', 'LDA', 'KNN', 'SVC', 'CART', 'GNB', 'LR', 'MLP']):
     np.random.seed(42)
-    models = ['LNR', 'LDA', 'KNN', 'SVC', 'CART', 'GNB', 'LR', 'MLP']
     if debug == 1:
         models = models[:2]
     log_results = []
