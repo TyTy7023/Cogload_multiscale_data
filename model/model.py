b65720d51abfaae218bc9295de4ca429ea55074c
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2024-11-19 16:56:44 +0700

    fix path in model.py

--- model/model.py
+++ model/model.py
@@ -30,7 +30,7 @@ from model_method_I import EnsembleModel_7GB
 
 def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 , debug = 0, models = ['ESVM','E7GB', 'MLP', 'LR', 'LDA', 'KNN', 'RF', 'AB', 'GB', 'SVC', 'XGB']):
     np.random.seed(42)
-    path = os.path.join(path)
+    path = os.path.dirname(path)
     if debug == 1:
         models = models[:2]
     log_results = []
