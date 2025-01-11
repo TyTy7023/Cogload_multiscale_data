05f4ddd95107c04d8794a583a5c6fcbdbdee18b9
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-11 13:37:17 +0700

    fixx

--- model/best_model.py
+++ model/best_model.py
@@ -67,6 +67,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, feature_remo
                 y_pred_prob = estimator.predict_proba(X_val_fold)[:, 1]
 
             elif model == 'MLP_Keras':
+                print('is it here')
                 from MLP_model import MLP
                 estimator = MLP.MLP_Keras()
                 estimator.fit(X_train_fold, y_train_fold, X_val_fold, y_val_fold, path)
