0f4bd341d020dcdf2dc345c084894b702880eddf
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-11 12:19:38 +0700

    fix save file

--- model/best_model.py
+++ model/best_model.py
@@ -109,7 +109,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, feature_remo
                 'Features_removing': [feature_remove],
                 'Accuracy': [acc]
             })
-            df_to_append.to_csv(path, index=False)
+            df_to_append.to_csv(f'{path}{index_name}_results_model.csv', index=False)
         else:
             df_to_append = pd.DataFrame({
             "model": model,
