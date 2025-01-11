d120ae8c67fcebc30bce3c7a53c1822eed408f5a
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-11 13:29:49 +0700

    test

--- model/best_model.py
+++ model/best_model.py
@@ -105,6 +105,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, feature_remo
             df.to_csv(f'{path}{index_name}_results_model.csv', index=False)
 
         df_existing = pd.read_csv(f'{path}{index_name}_results_model.csv')
+        print(df_existing)
         if df_existing.empty: 
             df_to_append = pd.DataFrame({
                 "model": model,
