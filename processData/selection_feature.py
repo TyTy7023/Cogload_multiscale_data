95526d086263cfe8794ee5bf19a31c2a97977183
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-13 10:18:45 +0700

    fix num of features for SFS in SBS

--- processData/selection_feature.py
+++ processData/selection_feature.py
@@ -101,7 +101,7 @@ class Feature_Selection:
                                                 X_test = X_test, 
                                                 y_train = y_train,
                                                 model = SVC(kernel='linear'),
-                                                k_features = 18, 
+                                                k_features = 19, 
                                                 forward = False,
                                                 floating = True
                                                 )
