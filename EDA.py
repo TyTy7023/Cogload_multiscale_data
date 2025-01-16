9882e6de009b3ee69eefa9f7cc2ecb0eaa4f7688
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-16 13:55:58 +0700

    test

--- EDA.py
+++ EDA.py
@@ -11,8 +11,9 @@ class EDA:
     @staticmethod
     def draw_ROC_models_read_file(models, y_test,path):
         df = pd.read_csv(path)
-        
-        s = np.array(df['Y Probs'])
+        if path == '/kaggle/working/log/results_model.csv':
+            df['Y Probs'] = df['Y Probs'].str.replace(",", ".").str.replace("\n", " ")
+            df['Y Probs'] = df['Y Probs'].apply(lambda x: [float(i) for i in x.split()])
         parsed_data = np.array(df['Y Probs'])
         # Xử lý các phần tử trong s_cleaned
         y_prob = []
